e8de3b370b44ed50db84a8e865e29290b75762c8
Maxipar03 <maxipar2003@gmail.com>
2024-12-03 00:32:39 -0300

    adding a schedule link

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -10,7 +10,7 @@ function Banner() {
                     <h1>Transform Your Space: <strong className='bannerStrongText'>Professional Construction Cleaning</strong> in Florida</h1>
                     <h3>Is your project completed and needs to be ready for handover? Our team of construction cleaning experts in Florida is here to assist you with any of these services: Rough Clean, Final Clean, Touch-Up.
                     </h3>
-                    <button>Schedule a meet</button>
+                    <a href="https://calendly.com/floridabuildservices/informative-meet" target="_blank">Schedule a meet</a>
                 </div>
                 <div className='contentImg'>
                     <img src={Worker} alt="" />
